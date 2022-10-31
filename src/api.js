7202455c1afdcb7bd68c3e22bc6098580c530903
FarcasAlex <farcas683@gmail.com>
2022-10-31 16:19:19 +0200

    working GeoBDCities api

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1dd7aa88femsh101c31b4edab463p159d8bjsn31266dc53298',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
